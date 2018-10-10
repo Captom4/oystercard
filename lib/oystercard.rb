d7acf38475513025d835f00cd48d236c87adf283
Tom <tombjh@hotmail.co.uk>
2018-10-10 12:52:26 +0100

    fixed error found in test from putting 'it' inside an 'it'

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,12 @@
-class Oystercard
-  
+class OysterCard
   MAXIMUM_BALANCE = 90
-  
   attr_reader :balance
-  
   def initialize
     @balance = 0
   end
-  
-  def top_up(amount)
-    fail 'Maximum balance passed' if amount + balance > MAXIMUM_BALANCE
-    @balance += balance
+
+  def top_up(ammount)
+    raise 'Maximum balance passed' if ammount + balance > MAXIMUM_BALANCE
+    @balance += ammount
   end
-  
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,22 +1,22 @@
 require 'rspec'
 require_relative '../lib/oystercard'
 
-describe Oystercard do
+describe OysterCard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
-  
+
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument}
-    
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
     it 'It can top up balance' do
-      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-      
+      expect { subject.top_up 1 }.to change { subject.balance }.by 1
+    end
+
     it 'Raises an error if the maximum amount is passed' do
-      maxium_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maxium_balance)
-      expect{ subject.top_up 1 }.to raise_error 'Maximum balance passed'
+      maximum_balance = OysterCard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect { subject.top_up 1 }.to raise_error 'Maximum balance passed'
     end
   end
-end
 end
