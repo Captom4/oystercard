533fbb091c5b9f1edf043c755e413bf3ab5be67d
Tom Hird <tombjh@hotmail.co.uk>
2018-10-10 11:11:03 +0000

    add latest update to code to fix problems on chapter 5

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,5 +8,9 @@ describe Oystercard do
   
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument}
+    
+    it 'It can top up balance' do
+      expect{ subject.top_up 1}.to change{ subject.balance}.by 1
+    end
   end
 end
